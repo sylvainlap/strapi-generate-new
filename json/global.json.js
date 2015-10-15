38c91de6face026ab1f09c81b41c097a28e2630d
pierreburgy <pierre.burgy@gmail.com>
2015-10-15 16:02:12 +0200

    Use single quotes in global.json.js

--- json/global.json.js
+++ json/global.json.js
@@ -14,42 +14,42 @@ const _ = require('lodash');
 module.exports = function dataForPackageJSON(scope) {
 
   return {
-    "static": true,
-    "views": false,
-    "websockets": true,
-    "favicon": {
-      "path": "favicon.ico",
-      "maxAge": 86400000
+    'static': true,
+    'views': false,
+    'websockets': true,
+    'favicon': {
+      'path': 'favicon.ico',
+      'maxAge': 86400000
     },
-    "i18n": {
-      "defaultLocale": "en",
-      "modes": [
-        "query",
-        "subdomain",
-        "cookie",
-        "header",
-        "url",
-        "tld"
+    'i18n': {
+      'defaultLocale': 'en',
+      'modes': [
+        'query',
+        'subdomain',
+        'cookie',
+        'header',
+        'url',
+        'tld'
       ],
-      "cookieName": "locale"
+      'cookieName': 'locale'
     },
-    "prefix": "",
-    "blueprints": {
-      "defaultLimit": 30,
-      "populate": true
+    'prefix': '',
+    'blueprints': {
+      'defaultLimit': 30,
+      'populate': true
     },
-    "globals": {
-      "models": true,
-      "strapi": true,
-      "async": true,
-      "_": true
+    'globals': {
+      'models': true,
+      'strapi': true,
+      'async': true,
+      '_': true
     },
-    "studio": {
-      "enabled": true,
-      "secretKey": ""
+    'studio': {
+      'enabled': true,
+      'secretKey': ''
     },
-    "users": {
-      "jwtSecret": scope.jwtSecret
+    'users': {
+      'jwtSecret': scope.jwtSecret
     }
   };
 };
