c0153f784f35073fc119f5a6e98ad045369e9e08
loicsaintroch <hack@loic.io>
2015-11-02 14:42:27 +0100

    Load config from API and specific environment

---
+++ json/studio.json.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * Expose studio config
+ */
+
+module.exports = function dataForStudioJSON(scope) {
+  return {
+    'studio': {
+      'enabled': true,
+      'secretKey': 'YOUR SECRET KEY HERE'
+    }
+  };
+};
